290bd2b32d34e8115484247aab1e909932cba404
Tatiana Perminova <perminovaxxi@mail.ru>
2022-03-28 21:37:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/voice_assistant/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.voice_assistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ListView
+import android.widget.SimpleAdapter
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG: String = "MainActivity"
+    lateinit var requiestInput: TextInputEditText
+    lateinit var podsAdapter: SimpleAdapter
+    val pods = mutableListOf<HashMap<String, String>>(
+        HashMap<String, String>().apply {
+            put("Title", "Title 1")
+            put("Content", "Content 1")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 2")
+            put("Content", "Content 2")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 3")
+            put("Content", "Content 3")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 4")
+            put("Content", "Content 4")
+        }
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+    fun initViews(){
+        val toolbar: MaterialToolbar= findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        requiestInput = findViewById(R.id.text_input_edit)
+
+        val podsList:ListView = findViewById(R.id.pods_list)
+        podsAdapter = SimpleAdapter(
+            applicationContext,
+            pods,
+            R.layout.item_pod,
+            arrayOf("Title", "Context"),
+            intArrayOf(R.id.title, R.id.content)
+
+        )
+        podsList.adapter = podsAdapter
+
+    }
+
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_stop -> {
+                Log.d(TAG, "action_stop")
+                return true
+            }
+            R.id.action_clear -> {
+                Log.d(TAG, "action_clear")
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
